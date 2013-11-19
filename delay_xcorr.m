047b19b9d6858255b4628ee2e587a4efc3ed8f89
dani <dani91.bcn@gmail.com>
2013-11-19 14:32:52 +0100

    prueba commit

--- delay_xcorr.m
+++ delay_xcorr.m
@@ -4,12 +4,13 @@ function estimated_delay = delay_xcorr( input1, input2 )
     % ¿qué hacemos si hay más de un máximo?
     %  (poco probable en señales reales) de momento he cogido el último
     
-    if length(input1) ~= length(input2)
+    if length(input2) ~= length(input1)
         error('- ¡Both inputs must be the same length to correlate them!');
     end
     cc = xcorr(input1,input2);
     [value, peak_positions] = max(cc);
     peak_position = max(peak_positions);
     estimated_delay = peak_position - length(input1);
+    cosa a�adida
 end
 
