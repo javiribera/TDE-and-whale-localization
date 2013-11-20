d4ffdaee0ed137a7092fb8097eb43f0bd78d6547
Javier Ribera <javiribera@gmail.com>
2013-11-20 23:35:14 +0100

    bugs importantes

--- delay_gcc.m
+++ delay_gcc.m
@@ -1,4 +1,4 @@
-function estimated_delay = delay_gcc( input1, input2 )
+function estimated_delay = delay_gcc( input1, input2, method )
     % Estima el delay de 2 señales usando la función
     % gcc de Dutroit y el máximo
     % ¿qué hacemos si hay más de un máximo?
@@ -7,7 +7,7 @@ function estimated_delay = delay_gcc( input1, input2 )
     if length(input1) ~= length(input2)
         error('- ¡Both inputs must be the same length to correlate them!');
     end
-    cc = gcc(input1,input2);
+    cc = gcc(input1,input2, method);
     [value, peak_positions] = max(cc);
     peak_position = max(peak_positions);
     estimated_delay = peak_position - length(input1);
--- gcc_on_test_signals.m
+++ gcc_on_test_signals.m
@@ -5,21 +5,21 @@ DEBUG=1;
 gcc_mode = 'phat';
 
 %% TDE on sinus using gcc and the maximum peak
-delay_sinus = delay_gcc(seno1, seno2);
+delay_sinus = delay_gcc(seno1, seno2, gcc_mode);
 
 if DEBUG
     figure
     hold on
     subplot(1,2,1)
-    plot(delay_sinus); title('gcc between sinus');
+    plot(gcc(seno1, seno2, gcc_mode)); title('gcc between sinus');
 end
 
-%% TDE on chirp using xcorr and the maximum peak
-delay_sinus = delay_gcc(chirp1, chirp2);
+%% TDE on chirp using gcc and the maximum peak
+delay_chirp = delay_gcc(chirp1, chirp2, gcc_mode);
 
 if DEBUG
     subplot(1,2,2)
-    plot(gcc_chirp); title('xcorr between chirps');
+    plot(gcc(chirp1, chirp2, gcc_mode)); title('gcc between chirps');
 end
 
 %% DEBUG: no need of aux. variables
