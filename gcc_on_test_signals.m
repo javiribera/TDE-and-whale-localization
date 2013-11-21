6776f9e8b443a06620ac0dbcd29817f7ac30e886
Javier Ribera <javiribera@gmail.com>
2013-11-21 21:53:38 +0100

    trying gcc on noise: phat :) on noise

--- gcc_on_test_signals.m
+++ gcc_on_test_signals.m
@@ -10,7 +10,7 @@ delay_sinus = delay_gcc(seno1, seno2, gcc_mode);
 if DEBUG
     figure
     hold on
-    subplot(1,2,1)
+    subplot(1,3,1)
     plot(gcc(seno1, seno2, gcc_mode)); title('gcc between sinus');
 end
 
@@ -18,11 +18,20 @@ end
 delay_chirp = delay_gcc(chirp1, chirp2, gcc_mode);
 
 if DEBUG
-    subplot(1,2,2)
+    subplot(1,3,2)
     plot(gcc(chirp1, chirp2, gcc_mode)); title('gcc between chirps');
+end
+
+
+%% TDE on chirp using gcc and the maximum peak
+delay_noise = delay_gcc(noise1, noise2, gcc_mode);
+
+if DEBUG
+    subplot(1,3,3)
+    plot(gcc(noise1, noise2, gcc_mode)); title('gcc between white noise');
 end
 
 %% DEBUG: no need of aux. variables
 if ~DEBUG
-    clear('DEBUG','peak*','xcorr*','gcc_mode')
+    clear('DEBUG','peak*','xcorr*','gcc_mode', 'noise*')
 end
