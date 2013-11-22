a4e200ec400ed6ff2d39c1279704cee3c6f08f25
Javier Ribera <javiribera@gmail.com>
2013-11-22 11:51:43 +0100

    better preconditioning filters

--- clean_signal.m
+++ clean_signal.m
@@ -6,16 +6,31 @@ function output = clean_signal( input )
     %% IN PROGRESS
 
     Fs=96e3; % 96 KHz
-
-    % notch filter (banda eliminada)
-    %  REQUIRES DSP system toolbox
-    freq_interference = 20e3; % 20 KHz
-    wo = freq_interference/(Fs/2);
-    Q = 35; % quality factor
-    bw = wo/Q;
-    [num_notch,den_notch] = iirnotch(wo,bw);
     
-    output = filter(num_notch, den_notch, input);
+        % hydrophone signal is not zero mean :0
+        signal1 = input - mean(input);
+
+        % notch filter (banda eliminada)
+        %  REQUIRES DSP system toolbox
+        freq_interference = 20e3; % 20 KHz
+        wo = freq_interference/(Fs/2);
+        Q = 2; % quality factor
+        bw = wo/Q;
+        [num_notch,den_notch] = iirnotch(wo,bw);    
+        signal2 = filter(num_notch, den_notch, signal1);
+
+        % Low-pass filter
+        %  REQUIRES Signal Processing toolbox
+        Fc    = 500; % Hz
+        fc = Fc / (Fs/2);
+        N = 200;   % FIR filter order
+        Hf = fdesign.lowpass('N,Fc',N,fc);
+        Hd = design(Hf,'window','window',{@chebwin,50},'SystemObject',true);
+        % hfvt = fvtool(Hd,'Color','White'); DEBUG: visualize filter
+        signal3=filter(Hd.Numerator,1,signal2);
+        
+    output=signal3;
+
 
 end
 
