318f812f2c1eb7c4437bb9c74cfb30df17605a7d
Javier Ribera <javiribera@gmail.com>
2013-12-04 21:24:04 +0100

    LMS bug fix + adaptive stepsize

--- delay_lms.m
+++ delay_lms.m
@@ -1,40 +1,52 @@
-function estimated_delay_samples = delay_lms(input1, input2, max_expected, stepsize)
+function estimated_delay_samples = delay_lms(input1, input2, max_expected, beta)
     % Estimates the delay between input1 and input2 using LMS adaptive method
-    % using the stepsize provided and max_expected in samples.
-%     clear; clc
-%     init_test_signals()
-%     load 'test_signals.mat';
-%     input1=noise1; input2=noise2;
-%     max_expected = ceil(1.5*real_delay_samples);
-%     stepsize=0.01;
-global h e L;
-    h=0;e=0; L=0;
+    % using the smoothing parameter provided and max_expected in samples.
+
+    if exist('DEBUG','var') && DEBUG
+        global h e L;  h=0;e=0; L=0;
+    end
+    
     N = length(input1);
     if(N ~= length(input2))
         disp('Both inputs must be the same length to use LMS TDE');
         return
     end
+    if(~(beta>0 && beta < 1))
+        disp('Smoothin parameter (4th one) must be between 0 and 1');
+        return
+    end
     
     % assure input vectors to be columns and
-    % normalize max signal amplitudes to +1 and
+    % normalize max signal amplitudes to +1
     input1 = input1(:)/max(input1);
     input2 = input2(:)/max(input2);
-    
+        
     % length of the filter, must be higher than max possible delay
     L = 2*max_expected;
     %initial state of the filter
-    h = zeros(L,1); h(max_expected+1)=0;
+    h = zeros(L,1); %h(max_expected+1)=1;
     
     % iterating for each time instant
+    est_power = mean(input2(1:L).^2); %initially
     for n=L:N
+        % show progress
+        if exist('DEBUG','var') && DEBUG
+            disp(['step ', num2str(n) ,' of', num2str(N)])
+        end
+        
         x2 = input2(n:-1:n-(L-1)); % the n-th L-length subvector of input2
-        d = h'*x2; % desired to be a mimic of x1 at this instant
+        
+        % adaptively choose stepsize depending on power signal
+        est_power = beta*est_power + (1-beta)*input2(n)^2;
+        stepsize = (1-beta)/(est_power);
+        
+        d = h'*x2; % to be a mimic of x1 at this instant
         
         e(n) = input1(n) - d; % error of the mimic at this instant
         
         h = h + 2*stepsize*e(n)*x2; % update the filter!
     end
     
-    [maximum pos] = max(h);
+    [~,pos] = max(h);
     
     estimated_delay_samples = pos-1;
