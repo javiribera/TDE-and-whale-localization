1adc3d5d01b7fd7391e6d6d0c5a079dbc89823e8
Javier Ribera <javiribera@gmail.com>
2013-12-04 10:40:36 +0100

    LMS bug fixes

--- delay_lms.m
+++ delay_lms.m
@@ -8,6 +8,7 @@ function estimated_delay_samples = delay_lms(input1, input2, max_expected, steps
 %     max_expected = ceil(1.5*real_delay_samples);
 %     stepsize=0.01;
 global h e L;
+    h=0;e=0; L=0;
     N = length(input1);
     if(N ~= length(input2))
         disp('Both inputs must be the same length to use LMS TDE');
@@ -22,11 +23,11 @@ global h e L;
     % length of the filter, must be higher than max possible delay
     L = 2*max_expected;
     %initial state of the filter
-    h = zeros(L,1); h(max_expected)=1;
+    h = zeros(L,1); h(max_expected+1)=0;
     
     % iterating for each time instant
     for n=L:N
-        x2 = flipud(input2(n-(L-1) : n)); % the n-th L-length subvector of input2
+        x2 = input2(n:-1:n-(L-1)); % the n-th L-length subvector of input2
         d = h'*x2; % desired to be a mimic of x1 at this instant
         
         e(n) = input1(n) - d; % error of the mimic at this instant
@@ -36,4 +37,4 @@ global h e L;
     
     [maximum pos] = max(h);
     
-    estimated_delay_samples = pos - max_expected;
+    estimated_delay_samples = pos-1;
