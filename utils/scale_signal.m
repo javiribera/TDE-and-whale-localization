1e96804690884e806555504f357bfa9eea760205
dani <dani91.bcn@gmail.com>
2013-12-07 21:27:52 +0100

    nada

--- utils/scale_signal.m
+++
@@ -1,9 +0,0 @@
-function output = scale_signal( input, lower, upper )
-%SCALE_SIGNAL Scales signal to range [min, max].
-%sound(scale_signal(data,-1,1)) is uequivalent to soundsc(data). It is
-%useful to maximize the speakers audio without distorsion
-
-    output = (input-min(input))*(upper-lower)/(max(input)-min(input))+lower;
-    
-end
-
