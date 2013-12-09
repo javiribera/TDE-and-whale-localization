61a5e85509d36757de71cb72208403d00bb69619
Javier Ribera <javiribera@gmail.com>
2013-12-09 10:00:28 +0100

    sin importancia

--- delay_xcorr.m
+++ delay_xcorr.m
@@ -1,8 +1,6 @@
 function estimated_delay = delay_xcorr( input1, input2 )
     % Estima el delay de 2 señales usando la función
     % xcorr de MATLAB y el máximo
-    % ¿qué hacemos si hay más de un máximo?
-    %  (poco probable en señales reales) de momento he cogido el último
     
     if length(input2) ~= length(input1)
         error('- ¡Both inputs must be the same length to correlate them!');
