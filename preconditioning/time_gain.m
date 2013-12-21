95e13389227eedea73ba7f4aebcd8ebbcde855e9
Javier Ribera <javiribera@gmail.com>
2013-12-21 11:52:21 +0100

    code cleanup

--- preconditioning/time_gain.m
+++ preconditioning/time_gain.m
@@ -7,10 +7,9 @@ function output = time_gain(input, params)
 
      % output overall power level
     aver_level(1) = input(1);
-    signal3(1) = r*input(1);
+    output(1) = r*input(1);
     for n=2:length(input)
         aver_level(n) = ( alpha*((aver_level(n-1)).^p) + (1-alpha)*(abs(input(n)).^p) ).^(1/p);
-        signal3(n) = r * max((input(n) /signal3(n-1)),eps);
+        output(n) = r * max((input(n) /output(n-1)),eps);
     end
-    output=signal3;
 end
